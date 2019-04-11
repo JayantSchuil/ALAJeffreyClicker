2eee9a30141692a53c78d6edcd746be1e288e0b0
Jayant Schuil <jayantschuil@hotmail.com>
2019-04-11 13:17:39 +0200

    setup branch

--- app/src/main/java/com/thebestamogroup/alajeffreyclicker/MainActivity.java
+++ app/src/main/java/com/thebestamogroup/alajeffreyclicker/MainActivity.java
@@ -1,13 +1,32 @@
 package com.thebestamogroup.alajeffreyclicker;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+    int num = 0;
+    TextView tView;
+    ImageButton clickHere;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tView = findViewById(R.id.counter);
+        clickHere = findViewById(R.id.smoel);
+
+        clickHere.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                String display = String.format(getString(R.string.counter, Integer.toString(++num)));
+                tView.setText(display);
+            }
+        });
+
     }
+}
 }
