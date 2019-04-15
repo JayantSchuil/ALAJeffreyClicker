b5a7d33a4d35dda038a4248ba7f7b4b0fc371ed7
Jayant Schuil <jayantschuil@hotmail.com>
2019-04-15 09:51:09 +0200

    initialised the ShæredPreferndeces

--- app/src/main/java/com/thebestamogroup/alajeffreyclicker/MainActivity.java
+++ app/src/main/java/com/thebestamogroup/alajeffreyclicker/MainActivity.java
@@ -2,31 +2,33 @@ package com.thebestamogroup.alajeffreyclicker;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.ImageButton;
-import android.widget.TextView;
-import android.view.View;
+import android.content.SharedPreferences;
 
 public class MainActivity extends AppCompatActivity {
 
-    int num = 0;
-    TextView tView;
-    ImageButton clickHere;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        SharedPreferences sharedPrefs = getSharedPreferences("player", MODE_PRIVATE);
+        SharedPreferences.Editor ed;
+        if(!sharedPrefs.contains("initialized")){
+            ed = sharedPrefs.edit();
 
-        tView = findViewById(R.id.counter);
-        clickHere = findViewById(R.id.smoel);
+            //Indicate that the default shared prefs have been set
+            ed.putBoolean("initialized", true);
 
-        clickHere.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                String display = String.format(getString(R.string.counter, Integer.toString(++num)));
-                tView.setText(display);
-            }
-        });
+            //Initialise the Shared Preferences
+            ed.putInt("Jeffrey Punten", 0);
+            ed.putInt("Motiverende Speech", 0);
+            ed.putInt("Nieuw Blazter Gebouw", 0);
+            ed.putInt("Sterkere Wing Chunners", 0);
+            ed.putInt("Duitse Upgrade", 0);
+            ed.putInt("Nieuwe Mercedez-Benz", 0);
+            ed.putInt("Niewe Baby", 0);
 
+            ed.commit();
+        }
     }
-}
 }
