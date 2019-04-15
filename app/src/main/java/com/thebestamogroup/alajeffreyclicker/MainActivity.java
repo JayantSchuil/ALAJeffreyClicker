516eb31aee488ecc25f5d4e6e8a1e069b75e9a1f
rick de bruijn <rick2.0@hotmail.nl>
2019-04-15 23:27:10 +0200

    counter werkt, jeffs gezicht werkt

--- app/src/main/java/com/thebestamogroup/alajeffreyclicker/MainActivity.java
+++ app/src/main/java/com/thebestamogroup/alajeffreyclicker/MainActivity.java
@@ -2,30 +2,57 @@ package com.thebestamogroup.alajeffreyclicker;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.content.SharedPreferences;
+import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
-import android.view.View;
+
 
 public class MainActivity extends AppCompatActivity {
 
-    int num = 0;
-    TextView tView;
-    ImageButton clickHere;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final SharedPreferences sharedPrefs = getSharedPreferences("player", MODE_PRIVATE);
+        final SharedPreferences.Editor ed;
+        if(!sharedPrefs.contains("initialized")){
+            ed = sharedPrefs.edit();
 
-        tView = findViewById(R.id.counter);
-        clickHere = findViewById(R.id.smoel);
+            //Indicate that the default shared prefs have been set
+            ed.putBoolean("initialized", true);
 
-        clickHere.setOnClickListener(new View.OnClickListener() {
+            //Initialise the Shared Preferences
+            ed.putInt("Jeffrey Punten", 0);
+            ed.putInt("Motiverende Speech", 0);
+            ed.putInt("Nieuw Blazter Gebouw", 0);
+            ed.putInt("Sterkere Wing Chunners", 0);
+            ed.putInt("Duitse Upgrade", 0);
+            ed.putInt("Nieuwe Mercedez-Benz", 0);
+            ed.putInt("Niewe Baby", 0);
+
+            ed.apply();
+        }
+        else{
+            //If initialized set counter to correct number
+            TextView textView1 = findViewById(R.id.counter);
+            textView1.setText(String.valueOf(sharedPrefs.getInt("Jeffrey Punten", 0)));
+        }
+
+        //OnClickListener for Jeffreys smoel
+        final ImageButton button = findViewById(R.id.smoel);
+        final TextView textView2 = findViewById(R.id.counter);
+        button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-
+                int currentJeffs = sharedPrefs.getInt("Jeffrey Punten", 0);
+                getSharedPreferences("player", MODE_PRIVATE)
+                        .edit()
+                        .putInt("Jeffrey Punten", currentJeffs+1)
+                        .apply();
+                textView2.setText(String.valueOf(sharedPrefs.getInt("Jeffrey Punten", 0)));
             }
         });
 
     }
 }
-
